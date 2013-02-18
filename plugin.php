63013ff4c10171d2fa9605c725a32d9f749320cd
Nicholas J Fantozz <mac1@nicks-imac-2.local>
2013-02-18 14:07:17 -0500

    Cleaned up code to production release

--- plugin.php
+++ plugin.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Lightbox By Mark Advertising
 Plugin URI: http://www.markadvertising.com
-Description: Lightbox
+Description: WordPress lightbox that utilizes WordPress included Thickbox.js
 Author: Mark Advertising
-Version: 1
+Version: 1.0.3
 Author URI: http://www.markadvertising.com
 */
 
@@ -31,16 +31,17 @@ function set_markad_lightbox_plugin_meta($links, $file) {
  
 
 /*
-	Enqueue scripts for everything everytime
-	
-	Not too sure if this is the best way
+	Enqueue scripts for front-end only
 */ 
 add_action('wp_enqueue_scripts', 'markad_lightbox_plugin_init');
 function markad_lightbox_plugin_init()
 {
-	wp_enqueue_style( 'thickbox' );
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'thickbox' );
+	if(!is_admin())
+	{
+		wp_enqueue_style( 'thickbox' );
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'thickbox' );
+	}
 }
 
 
@@ -48,8 +49,6 @@ function markad_lightbox_plugin_init()
 add_filter('the_content', 'markad_add_thickbox_lightbox', 2);
 function markad_add_thickbox_lightbox($content)
 {
-	/* Find internal links */
-
 	//Check the page for link images direct to image (no trailing attributes)
 	$string = '/<a href="(.*?).(jpg|jpeg|png|gif|bmp|ico)"><img(.*?)class="(.*?)wp-image-(.*?)" \/><\/a>/i';
 	preg_match_all( $string, $content, $matches, PREG_SET_ORDER);
@@ -87,60 +86,4 @@ function markad_attachment_link_filter( $content, $post_id, $size, $permalink ) 
     }
 	return $content;
 }
-/*
-	create custom plugin settings menu
-*/
-//add_action('admin_menu', 'markad_lightbox_plugin_create_menu');
-/*function markad_lightbox_plugin_create_menu() 
-{
-		//create new sub-level menu
-	add_submenu_page( 'options-general.php', 'Lightbox Settings', 'Lightbox', 'administrator',  __FILE__, 'markad_lightbox_plugin_settings_page' );
-
-	//call register settings function
-	add_action( 'admin_init', 'register_markad_google_map_plugin_settings' );
-}*/
-
-
-
-/*
-	register our settings
-
-function markad_lightbox_plugin_settings_page() 
-{
-	//register_setting( 'markad_lightbox_plugin_group', 'markad_lightbox_plugin_api_key' );
-}
-*/
-
-/*
-	Create settings page
-
-function markad_google_map_plugin_settings_page()
-{
-	?>
-	<div class="wrap">
-	<h2>Contact Form By Mark Advertising</h2>
-	
-	<form method="post" action="options.php">
-		<?php settings_fields( 'markad_lightbox_plugin_group' ); ?>
-		<?php do_settings_fields( 'markad_lightbox_plugin_group', '' ); ?>
-		<table class="form-table">
-		
-			<tr valign="top">
-				<th scope="row">Google Map API Key</th>
-				<td>
-            		<input type="text" name="markad_lightbox_plugin_api_key" value="<?php echo get_option('markad_lightbox_plugin_api_key'); ?>" size="50" />
-           		</td>
-			</tr>
-			
-		</table>
-		
-		<?php submit_button(); ?>
-	
-	</form>
-	</div>
-	<?php 
-}
-*/
-
-
 ?>
